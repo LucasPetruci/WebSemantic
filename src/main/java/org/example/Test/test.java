40e80a41ace54fac285cc2239eac2fd99b27c37e
LucasPetruci <lucaspetruci100@gmail.com>
2025-05-10 19:50:07 -0300

    feat: add 'owl:sameAs' and blank node

--- src/main/java/org/example/Test/test.java
+++ src/main/java/org/example/Test/test.java
@@ -1,18 +1,33 @@
 package org.example.Test;
 
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.*;
 
 import org.example.impl.SemanticCrawlerImpl;
 
 public class test {
 
 	public static void main(String[] args) {
-		String URI = "http://dbpedia.org/resource/Zico";
-		//String URI = "http://dbpedia.org/resource/Roger_Federer";	
+		System.setProperty("http.read.timeout", "10000");
+
+		String URI = "http://dbpedia.org/resource/Roger_Federer";
 		Model model = ModelFactory.createDefaultModel();			
 		SemanticCrawlerImpl ObjImpl = new SemanticCrawlerImpl();
-		ObjImpl.search(model, URI);		
+
+		System.out.println("Iniciando coleta de dados RDF para a URI: " + URI + "\n");
+		ObjImpl.search(model, URI);
+
+		// Mostra as primeiras triplas coletadas
+		System.out.println("\nExibindo até 20 triplas coletadas:\n");
+		StmtIterator it = model.listStatements();
+		int count = 0;
+		while (it.hasNext() && count < 20) {
+			Statement stmt = it.nextStatement();
+			System.out.println(stmt);
+			count++;
+		}
+
+		// Escreve todo o modelo em formato TTL
+		System.out.println("\nModelo completo em formato Turtle:\n");
 		model.write(System.out, "TTL");	
 	}
 }
--- src/main/java/org/example/impl/SemanticCrawlerImpl.java
+++ src/main/java/org/example/impl/SemanticCrawlerImpl.java
@@ -3,79 +3,86 @@ package org.example.impl;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.OWL;
-
 import org.example.Crawler.SemanticCrawler;
 
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
-import java.util.ArrayList;
-
 
 public class SemanticCrawlerImpl implements SemanticCrawler {
-	private ArrayList<String> URIsVisitadas = new ArrayList<>();
-	private CharsetEncoder enc = Charset.forName("ISO-8859-1").newEncoder(); //variável declaradas como atributo para economizar recurso ao não instanciar a cada passagem da estrutura de repetição
+	private Set<String> URIsVisitadas = new HashSet<>(); 
+	private CharsetEncoder enc = Charset.forName("ISO-8859-1").newEncoder();
 	private Model tempModel = ModelFactory.createDefaultModel();
-
-	private int successfulURICounter = 0;
-
+	
 	public SemanticCrawlerImpl() {}
 
 	public void search(Model model, String resourceURI) {
 		try {
-			tempModel.read(resourceURI); //Lê o RDF da URI num modelo temporário
-			StmtIterator getTriplas = tempModel.listStatements(tempModel.createResource(resourceURI),(Property)null,(RDFNode)null); //lista no statement apenas as triplas com o Recurso sendo URI em análise
-			model.add(getTriplas);	// Adiciona as triplas lidas ao modelo
-			URIsVisitadas.add(resourceURI); //adiciona a URI à lista de visitadas
-			StmtIterator statements = model.listStatements(model.createResource(resourceURI),OWL.sameAs,(RDFNode)null); //Coloca no statmentes apenas as triplas com o predicado OWN.sameAs
-			while (statements.hasNext()) { // varre as triplas do statment
-				Statement statement = statements.nextStatement(); //adiciona a tripla corrente em um statment
-				Resource object = (Resource) statement.getObject();	//recebe o objeto da tripla em análise
+			if (!enc.canEncode(resourceURI)) {
+				return;
+			}
 
-				//Verifica se URI já foi visitada para não adicioná-la novamente ao model
-				Set<String> set = new HashSet<String>(URIsVisitadas);
-				boolean isVisited = set.contains(object.getURI());
+			if (URIsVisitadas.contains(resourceURI)) {
+				return;
+			}
 
-				//Acompanhamento de informações no console, bloco pode ser colocado como comentário ou excluído para reduzir este tipo de informação
-				System.out.println("\nNova URI obtida: "+object.getURI());
-				if (isVisited) System.out.println("URI: já visitada\n");
+			URIsVisitadas.add(resourceURI);
+			tempModel.read(resourceURI); 
 
-				if (enc.canEncode(object.getURI()) && !isVisited && !object.isAnon()) { //Testa se a URI é composta apenas caracteres do alfabeto latino (ISO 8859-1), se não foi visitada antes e se não é um nó em branco
-					search(model, object.getURI());	//chama recursivamente o o método search para analisar a nova URI que é OWL.sameAs com o Sujeito
-					successfulURICounter++;
-				} else if (object.getURI() == null || object.isAnon()) { //Verifica se o Objéto é um nó em branco
-					System.out.println("\nNó em branco: "+object.getId().toString()+"\n");
-					trataNoEmBranco (model, object); //chama a função que adiciona nó em branco ao modelo
+			StmtIterator getTriplas = tempModel.listStatements(tempModel.createResource(resourceURI), null, (RDFNode) null);
+			model.add(getTriplas);
+
+			StmtIterator statements = tempModel.listStatements(tempModel.createResource(resourceURI), OWL.sameAs, (RDFNode) null);
+			while (statements.hasNext()) {
+				Statement statement = statements.nextStatement();
+				RDFNode object = statement.getObject();
+
+				if (object.isAnon()) {
+					trataNoEmBranco(model, (Resource) object);
+				} else if (object.isURIResource()) {
+					String objURI = object.asResource().getURI();
+					if (enc.canEncode(objURI) && !URIsVisitadas.contains(objURI)) {
+						search(model, objURI); 
+					}
 				}
 			}
-			//tratamento de erros
-		}catch (Exception e) {
-			System.err.println ("Erro ao tentar abrir URI: "+e.getMessage());
-		}catch (NoClassDefFoundError error) {
-			System.err.println ("Erro de classe não encontrada ao tentar abrir URI: "+error.getMessage());
+
+			StmtIterator reverseStatements = tempModel.listStatements(null, OWL.sameAs, tempModel.createResource(resourceURI));
+			while (reverseStatements.hasNext()) {
+				Statement reverseStmt = reverseStatements.nextStatement();
+				Resource subject = reverseStmt.getSubject();
+
+				if (subject.isAnon()) {
+					trataNoEmBranco(model, subject);
+				} else {
+					String subjURI = subject.getURI();
+					if (enc.canEncode(subjURI) && !URIsVisitadas.contains(subjURI)) {
+						search(model, subjURI);
+						
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Erro ao tentar abrir URI: " + e.getMessage());
+		} catch (NoClassDefFoundError error) {
+			System.err.println("Erro de classe não encontrada ao tentar abrir URI: " + error.getMessage());
 		}
-		System.out.println("URI visitadas com sucesso: "+successfulURICounter);
 	}
 
 	private void trataNoEmBranco(Model model, Resource object) {
-		StmtIterator stmSNosEmBranco = model.listStatements((Resource)object.getId(),(Property)null,(RDFNode)null); //lê todos as trilplas do nó em branco
-		while (stmSNosEmBranco.hasNext()) { //varre as triplas do nó em branco
-			Statement stmNoAtual = stmSNosEmBranco.nextStatement(); //Obtem a tripla atual do nó em branco
-			model.add((Resource) stmNoAtual.getObject(),(Property) stmNoAtual.getPredicate(), (RDFNode) stmNoAtual.getObject()); //adiciona a tripla ao modelo
-		}
-		stmSNosEmBranco = model.listStatements((Resource)object.getId(),(Property)null,(RDFNode)null); //lê novamente para procurar outros nós em branco não encontrei na documentação do Jena um procedimento que coloca o cursor no início
+		StmtIterator stmSNosEmBranco = tempModel.listStatements(object, null, (RDFNode) null);
 		while (stmSNosEmBranco.hasNext()) {
-			Statement stmNoAtual = stmSNosEmBranco.nextStatement(); //Obtem a tripla atual do nó em branco
-			Resource objectNo = (Resource) stmNoAtual.getObject(); //Obtém o objeto para análise e envio de parâmentro caso necessário
-			if (objectNo.isAnon()) { //Se o objeto é outro nó em branco
-				trataNoEmBranco(model,objectNo); //chama recursivamente o "trataNoEmBranco" enviando o novo nó em branco obtido
+			Statement stmNoAtual = stmSNosEmBranco.nextStatement();
+			model.add(stmNoAtual);
+		}
+
+		stmSNosEmBranco = tempModel.listStatements(object, null, (RDFNode) null);
+		while (stmSNosEmBranco.hasNext()) {
+			Statement stmNoAtual = stmSNosEmBranco.nextStatement();
+			RDFNode obj = stmNoAtual.getObject();
+			if (obj.isAnon()) {
+				trataNoEmBranco(model, (Resource) obj);
 			}
 		}
 	}
